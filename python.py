7e830209ebe65e079a6024d0da104a212c5989bd
kadir-zeren <k4dzeren@gmail.com>
2024-12-24 10:41:17 +0300

    practice

--- python.py
+++ python.py
@@ -2,3 +2,101 @@
 print('kullanici ismi') # Burada kullanici ismi yazddiriliyor.
 
 x = 5 # burada x degeri atandi
+
+# bu bir comment
+# bu ikinci comment
+# bu ucuncu yorum
+
+a = "bu benim yazdigim string \
+stringe devam ediyorum"
+
+a
+
+a = "bu benim yazdigim string \nstringe devam ediyorum"
+
+print(a)
+
+school = "clarusway"
+
+school
+
+print(school)
+
+sayi = 35
+
+print(sayi)
+
+ğğğ = 5
+
+print(ğğğ) # turkce karakter hata vermez, ama kullanmayin
+
+okul = 'Clarusway'
+
+help('keywords' )
+
+_okul = 'clarusway'
+
+variable = 'degisken'
+
+isim_soyisim = 'ali guzel'
+
+degisken = 'bu bir string'
+print(degisken)
+
+degisken = 5
+print(degisken)
+
+a = 'degisken_1'
+b = 5
+c = 7.6
+print(a,b,c)
+
+ali = None
+print(ali)
+
+a, b, c = 5, 7, 9
+print(a,b,c)
+
+# type()
+#type() fonksiyonunun "Docstring "ini oku!
+
+a = 'clarusway'
+type(a)
+
+sehir = '35'
+type(sehir)
+
+sayi1 = 40
+type(sayi1)
+
+a = 5.7
+type(a)
+
+
+b = True
+print(type(a), type (b))
+
+a = 123
+type(a)
+str(a)
+
+yeni_a = str(a)
+type (yeni_a)
+
+pi = '3.14'
+pi
+
+print(pi)
+
+a = 1.23
+type(a)
+
+int(a)
+
+int (234.987)
+
+tamsayi = 7
+
+type(tamsayi)
+
+float (tamsayi)
